a90e74e6f5dc2521e37c360b8b00531033516018
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-03-14 09:06:55 -0300

    Curso básico de JavaScript

---
+++ 04_JavaScript_Explorando_a_Linguagem/2-trabalhandoComVariaveis.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+Idade = 30;
+//JS é case sensitive
+
+console.log(idade);
+console.log(Idade);
+
+// Boas Práticas:
+// Não utilizar variáveis com mesmo nome mudando apenas o case.
+// Sempre utilizar as palavras reservadas ao declarar a variável.
+
+const nome = "Diogo"
+
+console.log(nome);
+
+// Não fazer:
+// mes = "Janeiro";
---
+++ 04_JavaScript_Explorando_a_Linguagem/5-atribuicaoVariáveis.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com atribuição de Variáveis");
+
+const primeiroNome = "Diogo";
+const sobrenome = "Santana";
+
+// console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; // declarando variável
+idade = 26;
+idade = idade + 1;
+console.log(idade);
