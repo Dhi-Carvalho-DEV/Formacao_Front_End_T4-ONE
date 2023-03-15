71b6e2d4f399caa047d11360e3b5a008b62c2b9d
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-03-15 10:47:19 -0300

    Finalização do curso

--- 04_JavaScript_Explorando_a_Linguagem/8-loops.js
+++ 04_JavaScript_Explorando_a_Linguagem/8-loops.js
@@ -5,7 +5,7 @@ const listaDeDestinos = new Array('Salvador', 'São Paulo', 'Rio de Janeiro')
 const idadeComprador = 18
 const estaAcompanhada = false
 let temPassagemComprada = false
-const destino = "Salvador"
+const destino = "São Paulo"
 
 console.log('\n Destinos possíveis:')
 console.log(listaDeDestinos)
@@ -24,3 +24,15 @@ while (contador < 3) {
 }
 
 console.log("Destino existe: ", destinoExiste)
+
+if (podeComprar && destinoExiste) {
+  console.log("Boa Viagem!!")
+} else {
+  console.log("Desculpe tivemos um erro!!");
+}
+
+for (let cont = 0; cont < 3; cont++) {
+  if (listaDeDestinos[contador] == destino) {
+    destinoExiste = true
+  }
+}
