6b46a9c07077b76dacb490a548397032c8b2fe01
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-03-24 11:16:03 -0300

    Utilização de módulos

--- 07_JavaScript_Programando_a_Orientacao_a_Objetos/index.js
+++ 07_JavaScript_Programando_a_Orientacao_a_Objetos/index.js
@@ -1,22 +1,32 @@
-class Cliente {
-  nome
-  cpf
-  agencia
-  saldo
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente()
-const cliente2 = new Cliente()
-
 cliente1.nome = 'Ricardo'
 cliente1.cpf = 11122233309
-cliente1.agencia = 1001
-cliente1.saldo = 0
-cliente1.rg = 123456789
 
+const cliente2 = new Cliente()
 cliente2.nome = 'Alice'
 cliente2.cpf = 88822233309
-cliente2.agencia = 1001
-cliente2.saldo = 0
 
-console.log(cliente1, cliente2)
+const contaCorrenteRicardo = new ContaCorrente()
+contaCorrenteRicardo.agencia = 1001
+contaCorrenteRicardo.cliente = cliente1
+
+const ContaCorrenteAlice = new ContaCorrente()
+ContaCorrenteAlice.agencia = 5666
+ContaCorrenteAlice.cliente = cliente2
+
+console.log(ContaCorrenteAlice)
+console.log(contaCorrenteRicardo)
+
+contaCorrenteRicardo.depositar(500)
+console.log(contaCorrenteRicardo)
+contaCorrenteRicardo.transferir(200, ContaCorrenteAlice)
+
+console.log(ContaCorrenteAlice)
+console.log(contaCorrenteRicardo)
+
+ContaCorrenteAlice.transferir(300, contaCorrenteRicardo)
+console.log(ContaCorrenteAlice)
+console.log(contaCorrenteRicardo)
