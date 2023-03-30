c6ae8f2cf30246d83fd7d578fb593584934bbc4a
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-03-30 10:47:39 -0300

    Criando classes e métodos abstratos

--- 08_JavaScript_Interfaces_e_Heranca_em_Orientacao_a_Objetos/Conta.js
+++ 08_JavaScript_Interfaces_e_Heranca_em_Orientacao_a_Objetos/Conta.js
@@ -1,11 +1,18 @@
+//Classe abstrata
 export class Conta {
   constructor(saldoInicial, cliente, agencia) {
+    if (this.constructor == Conta) {
+      throw new Error(
+        'Você não deveria instanciar um objeto do tipo Conta Diretamente, pois essa é uma classe abstata'
+      )
+    }
+
     this._saldo = saldoInicial
     this._cliente = cliente
     this._agencia = agencia
   }
 
-    set cliente(novoValor) {
+  set cliente(novoValor) {
     if (novoValor instanceof Cliente) {
       this._cliente = novoValor
     }
@@ -19,17 +26,22 @@ export class Conta {
     return this._saldo
   }
 
+  //Método abstrato
   sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor
-      return valor
+    throw new Error('O método Sacar da conta é abstrato')
+  }
+
+  _sacar(valor, taxa) {
+    const valorSacado = taxa * valor
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado
+      return valorSacado
     }
+
+    return 0
   }
 
   depositar(valor) {
-    if (valor <= 100) {
-      return
-    }
     this._saldo += valor
   }
 
