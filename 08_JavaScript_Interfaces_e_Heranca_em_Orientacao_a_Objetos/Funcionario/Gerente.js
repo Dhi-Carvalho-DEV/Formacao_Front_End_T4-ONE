2c53e395c2ef55ea34fd2399629265b6b9e90792
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-04-03 07:46:17 -0300

    Entendendo o conceito de interfaces

--- 08_JavaScript_Interfaces_e_Heranca_em_Orientacao_a_Objetos/Funcionario/Gerente.js
+++ 08_JavaScript_Interfaces_e_Heranca_em_Orientacao_a_Objetos/Funcionario/Gerente.js
@@ -1,8 +1,8 @@
-import { Funcionario } from "./Funcionario.js";
+import { Funcionario } from './Funcionario.js'
 
 export class Gerente extends Funcionario {
-  constructor(nome, cpf, salario) {
-    super(nome, cpf, salario)
+  constructor(nome, salario, cpf) {
+    super(nome, salario, cpf)
     this._bonificacao = 1.1
   }
 }
